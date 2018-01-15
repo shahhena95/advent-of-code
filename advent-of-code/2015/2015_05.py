de2e1ca2372f5206c642bcde6d18094ee6499506
hena <shahhena95@gmail.com>
2018-01-15 17:27:28 +0900

    Add day 05 part 2

--- advent-of-code/2015/2015_05.py
+++ advent-of-code/2015/2015_05.py
@@ -9,8 +9,8 @@ disallowed = ['ab', 'cd', 'pq', 'xy']
 
 def vowel_check(string):
     count = 0
-    for letter in vowels:
-        if letter in string:
+    for letter in string:
+        if letter in vowels:
             count += 1
 
     if count >= 3:
@@ -34,7 +34,7 @@ def check_disallowed(string):
     return True
 
 
-def solve(part=1):
+def part1():
     count = 0
     for string in instructions:
         if vowel_check(string) and double_letter_check(string) and check_disallowed(string):
@@ -42,7 +42,31 @@ def solve(part=1):
 
     return count
 
-# print solve()
-print vowel_check("aaa")
-print double_letter_check("aaa")
-print check_disallowed("aaa")
+print part1()
+
+
+def check_double_pairs(string):
+    for i in range(0, len(string)-1):
+        char = string[i:i+2]
+        if char in string[i:]:
+            if char in string[i+2:]:
+                return True
+    return False
+
+
+def check_in_between(string):
+    for i in range(0, len(string)-2):
+        if string[i] == string[i+2]:
+            return True
+    return False
+
+
+def part2():
+    count = 0
+    for string in instructions:
+        if check_double_pairs(string) and check_in_between(string):
+            count += 1
+
+    return count
+
+print part2()
